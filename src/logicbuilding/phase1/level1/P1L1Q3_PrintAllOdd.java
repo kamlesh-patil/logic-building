201a4eedd93a31dc7519f01d3cdb1bce2d9744c0
kamlesh-patil <kamleshpatil649@gmail.com>
2026-03-06 17:25:04 +0530

    print all odd numbers

---
+++ src/logicbuilding/phase1/level1/P1L1Q3_PrintAllOdd.java
@@ -0,0 +1,14 @@
+package logicbuilding.phase1.level1;
+
+/**
+ * 3. Print all odd numbers between 1 and 100.
+ *
+ * @author kamlesh patil
+ */
+public class P1L1Q3_PrintAllOdd {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.print(i);
+        }
+    }
+}
